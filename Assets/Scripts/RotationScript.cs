c62a60a691b5a714c74cf02a73e814ac386fa6df
Ricky Leong <rickyhhleong@gmail.com>
2018-04-06 15:08:16 +1200

    Commits progress up to stage where maze is built but script for rotating the maze is not complete.

---
+++ Assets/Scripts/RotationScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationScript : MonoBehaviour {
+
+    Transform m_trans;
+    int count;
+
+	// Use this for initialization
+	void Start () {
+        m_trans = gameObject.transform;
+        count = 0;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (count != 1000)
+        {
+            m_trans.Rotate(0, 1, 0);
+            count++;
+        }
+	}
+}
