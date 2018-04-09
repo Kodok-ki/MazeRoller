0fbd2fc904d4d662ea8f1fcf873e9234f6505124
Ricky Leong <rickyhhleong@gmail.com>
2018-04-09 18:25:57 +1200

    Commits completed prototype of the maze. Includes the final build for it.

--- Assets/Scripts/RotationScript.cs
+++ Assets/Scripts/RotationScript.cs
@@ -5,20 +5,54 @@ using UnityEngine;
 public class RotationScript : MonoBehaviour {
 
     Transform m_trans;
-    int count;
+    Quaternion m_rot;
+    float m_x;
+    float m_y;
+    float m_z;
+
 
 	// Use this for initialization
 	void Start () {
         m_trans = gameObject.transform;
-        count = 0;
+        m_trans.rotation = Quaternion.identity;
+        m_rot = m_trans.rotation;
+        m_x = 0; m_y = 0; m_z = 0;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (count != 1000)
+       if(Input.GetKey(KeyCode.A)){
+            //m_rot.x++;
+            //m_rot.x = Mathf.Clamp(m_rot.x, -30, 30);
+            //Debug.Log(m_rot.ToString());
+            m_z = Mathf.Clamp(m_z+=1, -20, 20);
+            //m_trans.Rotate(new Vector3(0, 0, 10));
+            m_trans.rotation = Quaternion.Euler(m_x, m_y, m_z);
+            //Debug.Log(m_z);
+       }
+       if (Input.GetKey(KeyCode.D))
         {
-            m_trans.Rotate(0, 1, 0);
-            count++;
-        }
-	}
+            //m_rot.x++;
+            //m_rot.x = Mathf.Clamp(m_rot.x, -30, 30);
+            //Debug.Log(m_rot.ToString());
+            m_z = Mathf.Clamp(m_z -= 1, -20, 20);
+            //m_trans.Rotate(new Vector3(0, 0, 10));
+            m_trans.rotation = Quaternion.Euler(m_x, m_y, m_z);
+           // Debug.Log(m_z);
+       }
+       if (Input.GetKey(KeyCode.W))
+        {
+            m_x = Mathf.Clamp(m_x += 1, -15, 15);
+            //m_trans.Rotate(new Vector3(0, 0, 10));
+            m_trans.rotation = Quaternion.Euler(m_x, m_y, m_z);
+           // Debug.Log(m_z);
+       }
+       if (Input.GetKey(KeyCode.S))
+        {
+            m_x = Mathf.Clamp(m_x -= 1, -15, 15);
+            //m_trans.Rotate(new Vector3(0, 0, 10));
+            m_trans.rotation = Quaternion.Euler(m_x, m_y, m_z);
+           // Debug.Log(m_z);
+       }
+    }
 }
---
+++ Assets/Scripts/StartCollision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {	}
+	
+	// Update is called once per frame
+	void Update () {	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Vector3 fRebound = (collision.impulse / Time.fixedDeltaTime) * 0.01f; //Total force halved to get force from floor onto ball and then x0.75 for energy loss.
+        Debug.Log(collision.relativeVelocity.ToString());
+        //Debug.Log(fRebound.ToString());
+        collision.rigidbody.AddForce(fRebound, ForceMode.VelocityChange);
+    }
+}
