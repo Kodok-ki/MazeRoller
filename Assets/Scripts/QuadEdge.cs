ef7cbf40abe6e095ac22e9a6ae2c7db1921a49ec
Ricky Leong <rickyhhleong@gmail.com>
2018-04-26 16:03:45 +1200

    Adds barebones classes required for maze generation algorithm along with the main scene

---
+++ Assets/Scripts/QuadEdge.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace MazeGen
+{
+    /// <summary>
+    /// Class to represent the edges in the graph and its dual used to represent the maze.
+    /// </summary>
+    public class QuadEdge
+    {
+        Node m_node1;
+        Node m_node2;
+        Dnode m_dnode1;
+        Dnode m_dnode2;
+
+        public QuadEdge(){
+            //Depends on how the maze is going to be instantiated
+        }
+    }
+
+    /// <summary>
+    /// Class to represent the nodes (vertices) in the graph.
+    /// </summary>
+    public class Node{
+        //var info;
+        bool m_visited;
+        Node m_from;
+        Node m_next;
+        HashSet<QuadEdge> m_quadEdges;
+
+        public Node(){
+            m_visited = false;
+        }
+
+        /// <summary>
+        /// Returns the set of quadedges a node is attached to. 
+        /// </summary>
+        public HashSet<QuadEdge> Edges{ get; }
+
+        public string ToString(){
+            return "";
+        }
+    }
+
+    /// <summary>
+    /// Class to represent the nodes (vertices) in the dual graph.
+    /// </summary>
+    public class Dnode
+    {
+        bool m_visited;
+        Dnode m_from;
+        Dnode m_next;
+        HashSet<QuadEdge> m_quadEdges;
+
+        public Dnode(){
+            m_visited = false;
+        }
+    }
+}
--- Assets/Scripts/RotationScript.cs
+++ Assets/Scripts/RotationScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class RotationScript : MonoBehaviour {
 
     Transform m_trans;
-    Quaternion m_rot;
+    //Quaternion m_rot;
     float m_x;
     float m_y;
     float m_z;
@@ -15,7 +15,7 @@ public class RotationScript : MonoBehaviour {
 	void Start () {
         m_trans = gameObject.transform;
         m_trans.rotation = Quaternion.identity;
-        m_rot = m_trans.rotation;
+        //m_rot = m_trans.rotation;
         m_x = 0; m_y = 0; m_z = 0;
     }
 	
