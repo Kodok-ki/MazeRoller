e50e63a3d6af3c2cb5b92a2e7f98db57f0671923
Ricky Leong <rickyhhleong@gmail.com>
2018-06-17 18:04:19 +1200

    Adds code for generating a modified matrix but not quite a maze

--- Assets/Scripts/MazeGeneration.cs
+++ Assets/Scripts/MazeGeneration.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using MazeGen;
+using DualGraph;
 using System;
 
 public class MazeGeneration : MonoBehaviour {
@@ -10,36 +10,57 @@ public class MazeGeneration : MonoBehaviour {
     HashSet<QuadEdge> m_Edges;  //All edges in the maze
     Dnode[,] m_Dnodes;          //All dual nodes in the maze
     int size;                   //Length of dimension in m_Nodes
+    Dnode root;                 //Start of maze
 
     void Start() {
+        CreateMaze();
+        root = m_Dnodes[0, 0];
+        Traverse();
+        GenerateObjects();
+        //GraphTest();
+    }
+	
+	void Update () {
+		
+	}
+
+    void CreateMaze(){
         size = 5;               //Matrix size setting
         m_Nodes = new Node[size, size];  //Can replace all references to m_Nodes[]'s length by storing a variable instead.
         m_Dnodes = new Dnode[size - 1, size - 1];
         m_Edges = new HashSet<QuadEdge>();
-        //TODO: Move creation implementation to another function
-        for (int i = 0; i < size; i++) 
+        for (int i = 0; i < size; i++)
         {
             for (int j = 0; j < size; j++)
             {
                 m_Nodes[i, j] = new Node();
                 QuadEdge horz = new QuadEdge();         //Might need to be declared outside of loop - Temp. variable referencing the current quadedge between adjacent row nodes
-                if (j != 0){           //Connect row nodes
+                if (j != 0)
+                {           //Connect row nodes
                     horz = new QuadEdge(m_Nodes[i, j - 1], m_Nodes[i, j]);
                     if (i < size - 1) { m_Dnodes[i, j - 1] = new Dnode(); }   //Inserts all Dnodes into dual array - Should be implemented without the extra condition
-                    m_Edges.Add(horz);
-                    GameObject edge = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
-                    edge.transform.position = new Vector3(2 * j - 1, 0, 2 * i);
-                }     
-                if (i != 0) { //Connect column nodes
+                    horz.pos = new Vector3D(2 * j - 1, 0, 2 * i);             //Assigns the QuadEdge a position relative to the nodes
+                    m_Edges.Add(horz);              
+                    //GameObject edge = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+                    //edge.transform.position = new Vector3(2 * j - 1, 0, 2 * i);
+                }
+                if (i != 0)
+                { //Connect column nodes
                     QuadEdge vert = new QuadEdge(m_Nodes[i - 1, j], m_Nodes[i, j]); //Might need to be declared outside of loop - Temp. variable referencing the current quadedge between adjacent column nodes
+                    vert.pos = new Vector3D(2 * j, 0, 2 * i - 1);
                     m_Edges.Add(vert);
-                    GameObject edge = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
-                    edge.transform.position = new Vector3(2 * j, 0, 2 * i-1);
-                    if (j > 0 && j < (size-1) && i < (size-1)){
+                    //GameObject edge = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+                    //edge.transform.position = new Vector3(2 * j, 0, 2 * i - 1);
+                    if (j > 0 && j < (size - 1) && i < (size - 1))
+                    {
                         try
                         {
-                            vert.setDnodes(m_Dnodes[i-1, j-1], m_Dnodes[i-1,j]); //Create connection between row dnodes
-                            horz.setDnodes(m_Dnodes[i-1, j-1], m_Dnodes[i, j-1]); //Create connection between column dnodes
+                            vert.SetDnodes(m_Dnodes[i - 1, j - 1], m_Dnodes[i - 1, j]); //Create connection between row dnodes
+                            horz.SetDnodes(m_Dnodes[i - 1, j - 1], m_Dnodes[i, j - 1]); //Create connection between column dnodes
+                            m_Dnodes[i - 1, j - 1].AddEdge(vert); //These 4 lines are tentative. Not sure if they add edges properly.
+                            m_Dnodes[i - 1, j - 1].AddEdge(horz);
+                            m_Dnodes[i - 1, j].AddEdge(vert);
+                            m_Dnodes[i, j - 1].AddEdge(horz);
                         }
                         catch (NullReferenceException e)
                         {
@@ -47,16 +68,10 @@ public class MazeGeneration : MonoBehaviour {
                             throw e;
                         }
                     }
-                }  
+                }
             }
         }
-        GraphTest();
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
     /*Method to check that the maze is correctly implemented by 
      * iterating through m_Nodes and m_Dnodes and generating
@@ -71,6 +86,42 @@ public class MazeGeneration : MonoBehaviour {
                 if(i < (size-1) && j < (size-1) && m_Dnodes[i,j] != null){
                     GameObject dnode = GameObject.CreatePrimitive(PrimitiveType.Sphere);
                     dnode.transform.position = new Vector3(2 * i + 1f, 0, 2 * j + 1f);
+                }
+            }
+        }
+    }
+
+    void GenerateObjects(){
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                if(m_Nodes[i,j] != null){
+                    GameObject node = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    node.transform.position = new Vector3(j * 2, 0, i * 2);
+                }
+            }
+        }
+        foreach(QuadEdge q in m_Edges){ //Create walls from the edges
+            if(q.Wall){
+                GameObject edge = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                edge.transform.position = new Vector3(q.pos.x, q.pos.y, q.pos.z);
+            }
+        }
+    }
+
+    void Traverse(){ //Starting with iterative implementation
+        Stack<Dnode> stk = new Stack<Dnode>();
+        stk.Push(root);
+        while (stk.Count != 0) {
+            Dnode currentNode = stk.Pop();
+            if (!currentNode.IsVisited())
+            {
+                currentNode.SetVisited();
+                if(currentNode.From != null){ currentNode.TurnOffWall(); }
+                foreach (QuadEdge q in currentNode.QuadEdges) {
+                    if (!q.FarDnode(currentNode).IsVisited()) { 
+                        stk.Push(q.FarDnode(currentNode));           // Push neighbour onto stack
+                        q.FarDnode(currentNode).From = currentNode;  // Set neighbour's m_from node to current node.
+                    }
                 }
             }
         }
--- Assets/Scripts/QuadEdge.cs
+++ Assets/Scripts/QuadEdge.cs
@@ -1,34 +1,86 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 
-namespace MazeGen
+namespace DualGraph
 {
     /// <summary>
     /// Class to represent the edges in the graph and its dual used to represent the maze.
     /// </summary>
     public class QuadEdge
     {
-        Node m_node1;
-        Node m_node2;
+        Node  m_node1;
+        Node  m_node2;
         Dnode m_dnode1;
         Dnode m_dnode2;
+        bool  m_wall = true;        //Not instantiating as true when implemented in the constructor for some reason.
+        bool  m_visited;
+        
+        public Vector3D pos;
 
-        //Constructor
-        public QuadEdge() { }
+        public bool Visited
+        {
+            get
+            {
+                return m_visited;
+            }
+
+            set
+            {
+                m_visited = value;
+            }
+        }
+
+        public bool Wall
+        {
+            get
+            {
+                return m_wall;
+            }
+        }
+
+        public bool isWall(){
+            return m_wall;
+        }
+
+        public QuadEdge() { 
+            //this.Wall = true;
+            m_visited = false;
+        }
         public QuadEdge(Node n1, Node n2){ m_node1 = n1; m_node2 = n2;}
 
-        public void setDnodes(Dnode n1, Dnode n2){ m_dnode1 = n1; m_dnode2 = n2;}
+        public void SetDnodes(Dnode n1, Dnode n2){ m_dnode1 = n1; m_dnode2 = n2;}
 
-        public Node GetNode1()   { return m_node1; }
-        public Node GetNode2()   { return m_node2; }
-        public Dnode GetDnode1() { return m_dnode1; }
-        public Dnode GetDnode2() { return m_dnode2; }
+        public Node GetNode1()   { return m_node1; }    //Necessary?
+        public Node GetNode2()   { return m_node2; }    //Necessary?
+        public Dnode GetDnode1() { return m_dnode1; }   //Necessary?
+        public Dnode GetDnode2() { return m_dnode2; }   //Necessary?
+
+        /// <summary>
+        /// Sets the quadedge such that it is no longer a wall.
+        /// </summary>
+        public void TurnOffWall(){ m_wall = false; }
+
+        /* public Node FarNode(Node from){
+            if (from == m_node1) { return m_node2; }
+            else{ return m_node1; }
+        } */
+
+        /// <summary>
+        /// Returns the dual node attached to the quadedge that's not "from".
+        /// </summary>
+        /// <param name="from">The Dnode from which this function is called from.</param>
+        /// <returns></returns>
+        public Dnode FarDnode(Dnode from){
+            if(from == m_dnode1){ return m_dnode2; }
+            else{ return m_dnode1; }
+        }
     }
 
     /// <summary>
     /// Class to represent the nodes (vertices) in the graph.
     /// </summary>
-    public class Node{ //TODO: Implement getter functions for quadedges and corresponding nodes
+    public class Node{
         //var info;
         bool m_visited;
         Node m_from;
@@ -44,9 +96,23 @@ namespace MazeGen
         /// </summary>
         public HashSet<QuadEdge> Edges{ get; }
 
-        public override string ToString(){
-            return "";
+        public Node From
+        {
+            get {   return m_from;    }
+            set {   m_from = value;   }
         }
+
+        public Node Next //Possibly not necessary
+        {   get {   return m_next;    }
+            set {   m_next = value;   }
+        }
+
+        public bool IsVisited()  { return m_visited;       }
+        public void SetVisited() { m_visited = !m_visited; }
+
+        /* public override string ToString(){
+            return "";
+        } */
     }
 
     /// <summary>
@@ -59,8 +125,96 @@ namespace MazeGen
         Dnode m_next;
         HashSet<QuadEdge> m_quadEdges;
 
-        public Dnode(){
+        public Dnode() {
             m_visited = false;
+            m_quadEdges = new HashSet<QuadEdge>();
+        }
+        
+        public Dnode From
+        {
+            get { return m_from; }
+            set { m_from = value; }
+        }
+
+        public Dnode Next //Possibly not necessary
+        {
+            get { return m_next; }
+            set { m_next = value; }
+        }
+
+        public HashSet<QuadEdge> QuadEdges //Might need cleaning up
+        {
+            get
+            {
+                return m_quadEdges;
+            }
+
+            set
+            {
+                m_quadEdges = value;
+            }
+        }
+
+        public void AddEdge(QuadEdge e){ m_quadEdges.Add(e); }
+
+        public bool IsVisited()  { return m_visited;       }
+        public void SetVisited() { m_visited = !m_visited; }
+
+        /// <summary>
+        /// Turns off the wall of Dnode and its m_from node.
+        /// </summary>
+        public void TurnOffWall(){
+            if(m_from != null && m_quadEdges != null){
+                foreach(QuadEdge q in m_quadEdges){
+                    if(q.FarDnode(this) == m_from){ q.TurnOffWall(); }
+                }
+            }
+        }
+
+        public QuadEdge GetRandomEdge(){
+            if(m_quadEdges != null){
+                int size = m_quadEdges.Count;
+                int rnd = new Random().Next(size);
+                int i = 0;
+                foreach (QuadEdge q in m_quadEdges) {
+                    if (i == rnd) { return q; }
+                    i++;
+                }
+            }
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Struct to store three values. Not to be used as a replacement for Unity's Vector3 if using Unity.
+    /// </summary>
+    public struct Vector3D{
+
+        public float x, y, z;
+
+        public Vector3D(float x, float y, float z){
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public static bool operator ==(Vector3D lhs, Vector3D rhs){
+            /* if(
+                 lhs.x == rhs.x &&
+                 lhs.y == rhs.y &&
+                 lhs.z == rhs.z
+             ){ return true; }
+             return false; */
+            return lhs.Equals(rhs);
+        }
+        public static bool operator !=(Vector3D lhs, Vector3D rhs){
+            /*if (
+                lhs.x != rhs.x ||
+                lhs.y != rhs.y ||
+                lhs.z != rhs.z
+            ) { return true; }
+            return false; */
+            return !lhs.Equals(rhs);
         }
     }
 }
